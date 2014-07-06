08acfec1fe7e4611a03b35ed81e0fe6f2a24c7d8
nadavTeachScape <nadav.somekh@teachscape.com>
2014-07-05 23:55:40 -0700

    c2

--- app.js
+++ app.js
@@ -2,6 +2,30 @@
  * Module dependencies.
  */
 
+function processFizzBuzzLogic( num ){
+    var retVal;
+    // FizzBuzz Logic
+    if ((num % 3 === 0) && (num % 5 === 0)) {
+        retVal = "FizzBuzz";
+    } else if (num % 3 === 0) {
+        retVal = "Fizz";
+    } else if (num % 5 === 0) {
+        retVal = "Buzz";
+    } else {
+        retVal = num;
+    }
+
+return retVal;
+}
+
+function fizzBuzzLoop(total){
+    var retVal = []
+    for (var i = 1; i <= total; i++) {
+        retVal.push(processFizzBuzzLogic(i));
+    }
+return retVal.join(' ')
+}
+
 var express = require('express'),
     path = require('path'),
     twilio = require('twilio');
@@ -9,25 +33,6 @@ var express = require('express'),
 var app = express();
 
 
-/*
-app.configure(function () {
-    app.set('port', process.env.PORT || 5000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
-});
-
-
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
-
-*/
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -46,98 +51,36 @@ else {
     config = require('./config');
 }
 
-//Quick Start Example 1 - Make an outbound call
-function qs1(request, response) {
-    var capability = new twilio.Capability(config.accountSid, config.authToken);
-
-    //This is a TwiML app SID configured with a voice URL
-    //https://www.twilio.com/user/account/apps
-    capability.allowClientOutgoing('APd0aa702e9a16856c36d4476075cc212c');
-
-    // Render an EJS template with the token and page title in context
-    // EJS template is found in views/qs1.ejs
-    response.render('qs1', {
-        title:'Hello Monkey 1',
-        token:capability.generate()
-    });
-}
-app.get('/123', qs1);
-app.get('/qs1', qs1);
-
-//Quick Start Example 2 - Hang up a call
-app.get('/qs2', function(request, response) {
-    var capability = new twilio.Capability(config.accountSid, config.authToken);
-    capability.allowClientOutgoing('APd0aa702e9a16856c36d4476075cc212c');
-
-    response.render('qs2', {
-        title:'Hello Monkey 2',
-        token:capability.generate()
-    });
-});
-
-//Quick Start Example 3 - Accept an incoming call
-app.get('/qs3', function(request, response) {
-    var capability = new twilio.Capability(config.accountSid, config.authToken);
-    capability.allowClientOutgoing('APd0aa702e9a16856c36d4476075cc212c');
-    capability.allowClientIncoming('jenny');
-
-    response.render('qs3', {
-        title:'Hello Monkey 3',
-        token:capability.generate()
-    });
-});
-
-//Quick Start Example 4 - Make outgoing calls from the browser
-app.get('/qs4', function(request, response) {
-    var capability = new twilio.Capability(config.accountSid, config.authToken);
-    capability.allowClientOutgoing('APd0aa702e9a16856c36d4476075cc212c');
-    capability.allowClientIncoming('jenny');
-
-    response.render('qs4', {
-        title:'Hello Monkey 4',
-        token:capability.generate()
-    });
-});
-
-//Twilio request authentication
-app.post('/twiml', function(req, res) {
-    if (twilio.validateExpressRequest(req, config.authToken)) {
-        var resp = new twilio.TwimlResponse();
-        resp.say('express sez - hello twilio!');
-
-        res.type('text/xml');
-        res.send(resp.toString());
-    }
-    else {
-        res.send('you are not twilio.  Buzz off.');
-    }
-});
 
 app.get('/', function(req, res) {
        var resp = new twilio.TwimlResponse();
+       console.log('hit1')
              resp.say('Enter a number and then press *')
                 .gather({
                     action:'/fizzbuzz1',
-                    finishOnKey:'*'
+                            finishOnKey:'*',
+                            method:"GET"
                 });
                  res.set('Content-Type', 'text/xml');
+
                 res.send(resp.toString());
 });
 app.get('/fizzbuzz1', function(req, res) {
+        console.log('hit-fizbuzz')
        console.log("query", req.query)
 
        var resp = new twilio.TwimlResponse();
         var output = []
         var number = parseInt(req.query.Digits)
-        output.push(number)
+        output.push(fizzBuzzLoop(number));
 
              resp.say(output.join(' '));
                 res.set('Content-Type', 'text/xml');
                 res.send(resp.toString());
 });
 
-
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
