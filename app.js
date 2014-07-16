957e5b5291527306a96fc20e37170b45cf822fac
nadavTeachScape <nadav.somekh@teachscape.com>
2014-07-16 08:10:57 -0700

    start monogod

--- app.js
+++ app.js
@@ -10,12 +10,55 @@ var express = require('express'),
     twilio = require('twilio');
 
 var app = express();
+var Q = require("q");
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views')
 
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+
+var TwillioLog = {
+    collection: function(){
+    var deferred = Q.defer();
+    MongoClient.connect("mongodb://localhost:27017/twilioLogDb", function(err, db) {
+        if(!err) {
+            console.log("We are connected");
+        }else{
+            console.log("mongodb connection error", err)
+        }
+        db.collection('twilio', function(err, collection) {});
+        var collection = db.collection('log');
+        deferred.resolve(collection);
+    });
+    return deferred.promise;
+},
+    create: function(newEntry){
+        var deferred = Q.defer();
+            TwillioLog.collection().then(function(collection){
+            collection.insert(newEntry, function(err,row){
+                deferred.resolve(row._id);
+            })
+            });
+        return deferred.promise;
+    },
+    list: function(){
+        var deferred = Q.defer();
+
+        TwillioLog.collection().then(function(collection){
+            collection.find().toArray(function(err, items) {
+                deferred.resolve(items);
+            })
+         })
+        return deferred.promise;
+    }
+//
+}
+
 
 //Twilio goodness
 var config = {};
@@ -114,7 +157,74 @@ var phase3 = {
             {  phonenumber: phonenumber, delay:  req.query.delay}
         )
     }
+};////
+var phase4 = {
+    phase4: function(req, res) {
+//        TwillioLog.collection().then(function(){
+//            console.log('responseData : %j', arguments );
+//        })
+//
+//        var newEntry = {'phonenumber':'vvvvvv', delay: 9999191919};
+//        TwillioLog.create(newEntry)
+        TwillioLog.list().then(function(twilioLog){
+//                console.log(twilioLog)
+                res.render('phase4/index',
+                    {  to: config.to, twilioLog: twilioLog });
+        });
+    },
+    submitPhase4: function(req, res) {
+        var phonenumber = req.query.phonenumber;
+        var delay = req.query.delay;
+        //create db row, missing insert id, append that later
+        TwillioLog.create({phonenumber: phonenumber, delay: delay}).then(function(row){
+            var delayMillisecond = parseInt(delay) * 1000;
+            setTimeout(function(){phase4.makeCall(row)}, delayMillisecond);
+        });
+        res.render('phase4/submit',
+            {  phonenumber: phonenumber, delay:  req.query.delay}
+        )
+    },
+    phase4InitFizzBizz: function(req, res) {
+        res.set('Content-Type', 'text/xml');
+        res.send(phase4.queryFizzBuzzDigits());
+    },
+    makeCall: function(row){
+        client.makeCall({
+            to: row.phonenumber,
+            from: config.from,
+            url: config.url+'/phas4InitFizzBizz?id='+row._id
+
+        }, function(err, responseData) {
+
+            //executed when the call has been initiated.
+            console.log('err : %j', err);
+            console.log('responseData : %j', responseData);
+        });
+    },
+    doFizzbuzz: function(req, res) {
+
+    var resp = new twilio.TwimlResponse();
+    var output = []
+    var number = parseInt(req.query.Digits)
+
+    output.push(FizzBuzz.fizzBuzzLoop(number));
+
+    resp.say(output.join(' '));
+    res.set('Content-Type', 'text/xml');
+    res.send(resp.toString());
+    },
+    queryFizzBuzzDigits:  function(){
+        var resp = new twilio.TwimlResponse();
+        resp.say('Enter a number and then press *')
+            .gather({
+                action:'/doFizzbuzz4',
+                finishOnKey:'*',
+                method:"GET"
+            });
+        return resp.toString();
+    }
 };
+
 
 /////
 //phase1
@@ -130,6 +240,11 @@ app.post('/phase2InitFizzBizz',phase2.phase2InitFizzBizz)
 //phase3
 app.get('/phase3', phase3.phase3)
 app.get('/submitPhase3', phase3.submitPhase3)
+
+//phase4
+app.get('/phase4', phase4.phase4)
+app.post('/phase4InitFizzBizz',phase4.phase4InitFizzBizz)
+app.get('/submitPhase4', phase4.submitPhase4)
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
